05a43581109c5f6ab3f1cd6b77b1f094f3a93d81
escalopa <ahmad.helaly.dev@gmail.com>
2022-11-25 18:45:48 +0300

    add load configuration viper

---
+++ utils/config.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+type DBConfig struct {
+	ConnectionString string `mapstructure:"connection_string"`
+	Driver           string `mapstructure:"driver"`
+}
+
+type AppConfig struct {
+	Port int `mapstructure:"port"`
+}
+
+type Config struct {
+	App AppConfig `mapstructure:"app"`
+	DB  DBConfig  `mapstructure:"database"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
