22e017279eac8ea19a23642a6e5a0d3fde33ae39
escalopa <ahmad.helaly.dev@gmail.com>
2022-12-02 02:57:22 +0300

    add db docs

---
+++ dbdocs/schema.sql
@@ -0,0 +1,64 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+ALTER TABLE "entries"
+ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+CREATE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "entries" ("account_id");
+CREATE INDEX ON "transfers" ("from_account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+--
+--
+--
+CREATE TABLE "users" (
+"username" varchar PRIMARY KEY,
+"hashed_password" varchar NOT NULL,
+"full_name" varchar NOT NULL,
+"email" varchar UNIQUE NOT NULL,
+"password_changed_at" timestamptz NOT NULL DEFAULT ('0001-01-01 00:00:00Z'),
+"created_at" timestamptz NOT NULL DEFAULT (now())
+);
+ALTER TABLE "accounts"
+ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+-- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+ALTER TABLE "accounts"
+ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
+--
+--
+--
+CREATE TABLE "sessions" (
+"id" uuid NOT NULL,
+"username" varchar NOT NULL,
+"refresh_token" varchar NOT NULL,
+"is_blocked" boolean NOT NULL DEFAULT false,
+"user_agent" varchar NOT NULL,
+"client_ip" varchar NOT NULL,
+"expires_at" timestamptz NOT NULL,
+"created_at" timestamptz NOT NULL DEFAULT (now())
+);
+ALTER TABLE "sessions"
+ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
